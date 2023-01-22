3b06f7bbff2988fc96a74fa18c6204d11d6417f0
shadab <khanofficial7229@gmail.com>
2023-01-22 15:50:27 +0530

    spinner

---
+++ Spin.js
@@ -0,0 +1,10 @@
+
+
+startbtn.addEventListener("click", function () {
+    ghoom.style.animation = " ghoom infinite 10s linear";
+
+});
+
+stopbtn.addEventListener("click", function () {
+    ghoom.style.animation = "";
+});
